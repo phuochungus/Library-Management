a5904b6a2af709480e2d22d06ae1b0c38e41f086
phuochungus <21520252@gm.uit.edu.vn>
2023-05-25 19:22:44 +0700

    add get reserve day api

--- src/rules/rules.controller.ts
+++ src/rules/rules.controller.ts
@@ -1,5 +1,5 @@
 import { Controller } from '@nestjs/common';
-import { Body, Patch, UseGuards } from '@nestjs/common/decorators';
+import { Body, Get, Patch, UseGuards } from '@nestjs/common/decorators';
 import UpdateRuleDto from './dto/update-rule.dto';
 import { RulesService } from './rules.service';
 import { Roles } from 'src/auth/authorization/roles.decorator';
@@ -7,23 +7,24 @@ import { JwtAuthGuard } from 'src/auth/authentication/jwt-auth.guard';
 import { Role } from 'src/auth/authorization/role.enum';
 import { RolesGuard } from 'src/auth/authorization/roles.guard';
 
-
-@Roles(Role.Admin)
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('rules')
 export class RulesController {
   constructor(private rulesService: RulesService) {}
 
+  @Roles(Role.Admin)
   @Patch('/min_age')
   async updateMinAge(@Body() updateRuleDto: UpdateRuleDto) {
     await this.rulesService.updateRule('MINIMUM_AGE', updateRuleDto.value);
   }
 
+  @Roles(Role.Admin)
   @Patch('/max_age')
   async updateMaxAge(@Body() updateRuleDto: UpdateRuleDto) {
     await this.rulesService.updateRule('MAXIMUM_AGE', updateRuleDto.value);
   }
 
+  @Roles(Role.Admin)
   @Patch('/max_publish_year')
   async updateMaximumPublishYearSince(@Body() updateRuleDto: UpdateRuleDto) {
     await this.rulesService.updateRule(
@@ -32,16 +33,19 @@ export class RulesController {
     );
   }
 
+  @Roles(Role.Admin)
   @Patch('/max_borrow')
   async updateBorrowMax(@Body() updateRuleDto: UpdateRuleDto) {
     await this.rulesService.updateRule('BORROW_MAX', updateRuleDto.value);
   }
 
+  @Roles(Role.Admin)
   @Patch('/fine_per_day')
   async updateFinePerDay(@Body() updateRuleDto: UpdateRuleDto) {
     await this.rulesService.updateRule('FINE_PER_DAY', updateRuleDto.value);
   }
 
+  @Roles(Role.Admin)
   @Patch('/valid_period_of_user')
   async updateValidPeriodByDayOfUserAccount(
     @Body() updateRuleDto: UpdateRuleDto,
@@ -52,13 +56,15 @@ export class RulesController {
     );
   }
 
+  @Roles(Role.Admin)
   @Patch('/due_by_days')
-  async updateDueByDaysOfBorrow(
-    @Body() updateRuleDto: UpdateRuleDto,
-  ) {
-    await this.rulesService.updateRule(
-      'DUE_BY_DAYS',
-      updateRuleDto.value,
-    );
+  async updateDueByDaysOfBorrow(@Body() updateRuleDto: UpdateRuleDto) {
+    await this.rulesService.updateRule('DUE_BY_DAYS', updateRuleDto.value);
+  }
+
+  @Roles(Role.User)
+  @Get('/reserve_day')
+  async getReserveDay() {
+    return this.rulesService.getRule('RESERVE_DAY');
   }
 }
