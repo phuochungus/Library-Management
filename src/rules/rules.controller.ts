29a70ff08bd8a9d9f689027483ed539d5269775f
phuochungus <21520252@gm.uit.edu.vn>
2023-06-04 16:32:14 +0700

    remove auth guard for GET method

--- src/rules/rules.controller.ts
+++ src/rules/rules.controller.ts
@@ -7,35 +7,35 @@ import { JwtAuthGuard } from 'src/auth/authentication/jwt-auth.guard';
 import { Role } from 'src/auth/authorization/role.enum';
 import { RolesGuard } from 'src/auth/authorization/roles.guard';
 
-@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('rules')
 export class RulesController {
   constructor(private rulesService: RulesService) {}
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.Admin)
   @Patch('/min_age')
   async updateMinAge(@Body() updateRuleDto: UpdateRuleDto) {
     await this.rulesService.updateRule('MINIMUM_AGE', updateRuleDto.value);
   }
 
-  @Roles(Role.Admin)
   @Get('/min_age')
   getMinAge() {
     return this.rulesService.getRule('MINIMUM_AGE');
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.Admin)
   @Patch('/max_age')
   async updateMaxAge(@Body() updateRuleDto: UpdateRuleDto) {
     await this.rulesService.updateRule('MAXIMUM_AGE', updateRuleDto.value);
   }
 
-  @Roles(Role.Admin)
   @Get('/max_age')
   getMaxAge() {
     return this.rulesService.getRule('MAXIMUM_AGE');
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.Admin)
   @Patch('/max_publish_year')
   async updateMaximumPublishYearSince(@Body() updateRuleDto: UpdateRuleDto) {
@@ -45,35 +45,36 @@ export class RulesController {
     );
   }
 
-  @Roles(Role.Admin)
   @Get('/max_publish_year')
   getMaximumPublishYearSince() {
     return this.rulesService.getRule('MAXIMUM_PUBLISH_YEAR_SINCE');
   }
 
   @Roles(Role.Admin)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Patch('/max_borrow')
   async updateBorrowMax(@Body() updateRuleDto: UpdateRuleDto) {
     await this.rulesService.updateRule('BORROW_MAX', updateRuleDto.value);
   }
 
-  @Roles(Role.Admin)
   @Get('/max_borrow')
   getBorrowMax() {
     return this.rulesService.getRule('BORROW_MAX');
   }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.Admin)
   @Patch('/fine_per_day')
   async updateFinePerDay(@Body() updateRuleDto: UpdateRuleDto) {
     await this.rulesService.updateRule('FINE_PER_DAY', updateRuleDto.value);
   }
 
-  @Roles(Role.Admin)
   @Get('/fine_per_day')
   getFinePerDay() {
     return this.rulesService.getRule('FINE_PER_DAY');
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.Admin)
   @Patch('/valid_period_of_user')
   async updateValidPeriodByDayOfUserAccount(
@@ -85,25 +86,23 @@ export class RulesController {
     );
   }
 
-  @Roles(Role.Admin)
   @Get('/fine_per_day')
   getValidPeriodByDayOfUserAccount() {
     return this.rulesService.getRule('VALID_PERIOD_BY_DAY_OF_USER_ACCOUNT');
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.Admin)
   @Patch('/due_by_days')
   async updateDueByDaysOfBorrow(@Body() updateRuleDto: UpdateRuleDto) {
     await this.rulesService.updateRule('DUE_BY_DAYS', updateRuleDto.value);
   }
 
-  @Roles(Role.Admin)
   @Get('/due_by_days')
   getDueByDaysOfBorrow() {
     return this.rulesService.getRule('DUE_BY_DAYS');
   }
 
-  @Roles(Role.User)
   @Get('/reserve_day')
   async getReserveDay() {
     return this.rulesService.getRule('RESERVE_DAY');
