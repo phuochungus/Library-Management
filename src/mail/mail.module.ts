cd47661347768039449cfbfa674da7e31a86c501
phuochungus <21520252@gm.uit.edu.vn>
2023-06-06 00:02:08 +0700

    remove unuse import

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
-import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   providers: [MailService],
