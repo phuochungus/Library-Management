6e7cbbaa1083a9bc02ae3cc626f04f6b30bfb8fc
phuochungus <21520252@gm.uit.edu.vn>
2023-06-06 00:04:51 +0700

    remove unuse import

--- src/business-validate/business-validate.module.ts
+++ src/business-validate/business-validate.module.ts
@@ -1,7 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import Book from 'src/entities/Book';
-import User from 'src/entities/User';
 import { RulesModule } from 'src/rules/rules.module';
 import BusinessValidateService from './business-validate.service';
 
